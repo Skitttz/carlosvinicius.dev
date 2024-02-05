4bff9acc4234eff3c2e8b02b961e5b5a0ce0e188
Skitttz <skittz.dev@gmail.com>
2024-02-05 16:37:45 -0300

    refact: list repository 1/3

--- js/modules/listRepository.js
+++ js/modules/listRepository.js
@@ -3,50 +3,62 @@ import * as DOMPurify from 'dompurify';
 
 export default class RepositoryGit {
   constructor(bodyRepository) {
-    this.bodyRepository = document.querySelector(bodyRepository);
-  }
-  getDataUserGit(username) {
-    fetch(`https://api.github.com/users/${username}`, {})
-      .then((response) => response.json())
-      .then((data) => {
-        const numRepos = data.public_repos;
-        return numRepos;
-      })
-      .catch((error) => console.error(error));
+    this.bodyRepository = document.querySelectorAll(bodyRepository);
   }
 
-  getDataPinnedRepository(username) {
-    fetch(` https://pinned.berrysauce.me/get/${username}`, {})
-      .then((response) => response.json())
-      .then((data) => {
-        return data.map((item) =>
-          this.getDataPinnedRepository(username, item.name, 'main'),
-        );
-      })
-      .catch((error) => console.error(error));
+  async getDataUserGit(username) {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const data = await response.json();
+      const numRepos = data.public_repos;
+      return numRepos;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  getDataReadmeRepository(owner, repo, branch) {
-    fetch(
-      `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/README.md
-    `,
-      {},
-    )
-      .then((response) => response.text())
-      .then((data) => {
-        const dataReadMe = data;
-        this.bodyRepository.innerHTML = DOMPurify.sanitize(
-          marked.parse(dataReadMe),
-        );
-      });
+  async getDataPinnedRepository(username) {
+    try {
+      const response = await fetch(
+        `https://pinned.berrysauce.me/get/${username}`,
+      );
+      const data = await response.json();
+
+      for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+        if (item.name && username) {
+          const readmeData = await this.getDataReadmeRepository(
+            username,
+            item.name,
+            'main',
+            index,
+          );
+          console.log(readmeData);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  init() {
+  async getDataReadmeRepository(owner, repo, branch, index) {
+    try {
+      const response = await fetch(
+        `https://raw.githubusercontent.com/${owner}/${repo}/${branch}/README.md`,
+      );
+      const data = await response.text();
+      const sanitizedData = DOMPurify.sanitize(marked.parse(data));
+      this.bodyRepository[index].innerHTML = sanitizedData;
+      return sanitizedData;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async init() {
     const username = 'skitttz';
     if (username) {
-      this.getDataUserGit(username);
-      this.getDataPinnedRepository(username);
-      this.getDataReadmeRepository(username, 'Nights4Films', 'main');
+      await this.getDataPinnedRepository(username);
     }
   }
 }
