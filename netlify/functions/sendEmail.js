4ccf7b4966c60fe57e5ad279021d051ee5c02983
SKitttz <skittz.dev@gmail.com>
2024-04-22 09:18:00 -0300

    refact: sanitize & sendEmail ⌋ style: change menu color ⌋ chore: emailJS/node

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,58 +1,51 @@
-import emailjs from '@emailjs/browser';
-import fetch from 'node-fetch';
+import emailjs from "@emailjs/nodejs";
 
 export const handler = async (event) => {
+  console.log(event);
   const {
-    valueInputName,
-    valueInputEmail,
-    valueInputSubject,
-    valueInputMessage,
-  } = JSON.parse(event.body);
-
-  const { API_EMAIL_SERVICE_ID, API_EMAIL_PUBLIC_KEY, API_EMAIL_TEMPLATE_ID } =
-    process.env;
-
+    API_EMAIL_SERVICE_ID,
+    API_EMAIL_PUBLIC_KEY,
+    API_EMAIL_PRIVATE_KEY,
+    API_EMAIL_TEMPLATE_ID,
+  } = process.env;
+  let body = {};
+  body = JSON.parse(event.body);
   if (
-    valueInputName &&
-    valueInputEmail &&
-    valueInputSubject &&
-    valueInputMessage
+    body.nameValue &&
+    body.emailValue &&
+    body.subjectValue &&
+    body.messageValue
   ) {
-    const nameValue = valueInputName;
-    const emailValue = valueInputEmail;
-    const subjectValue = valueInputSubject;
-    const messageValue = valueInputMessage;
-
     const templateParams = {
-      from_name: nameValue,
-      subject: subjectValue,
-      message: messageValue,
-      from_email: emailValue,
+      from_name: body.nameValue,
+      subject: body.subjectValue,
+      message: body.messageValue,
+      from_email: body.emailValue,
     };
-
     try {
       emailjs.send(
         API_EMAIL_SERVICE_ID,
         API_EMAIL_TEMPLATE_ID,
         templateParams,
-        API_EMAIL_PUBLIC_KEY,
+        {
+          publicKey: API_EMAIL_PUBLIC_KEY,
+          privateKey: API_EMAIL_PRIVATE_KEY,
+        },
       );
-
       return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'E-mail enviado com sucesso!' }),
+        body: JSON.stringify({ message: "E-mail enviado com sucesso!" }),
       };
     } catch (error) {
-      console.error(error);
       return {
         statusCode: 500,
-        body: JSON.stringify({ message: 'Erro ao enviar o e-mail' }),
+        body: JSON.stringify({ message: "Erro ao enviar o e-mail" }),
       };
     }
   } else {
     return {
       statusCode: 400,
-      body: JSON.stringify({ message: 'Preencha todos os campos' }),
+      body: JSON.stringify({ message: "Preencha todos os campos" }),
     };
   }
 };
