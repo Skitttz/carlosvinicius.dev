27e9fd9aaebe1952b9528bd03f0b7fce8c536959
Skittz <skittz.dev@gmail.com>
2024-04-23 23:12:06 -0300

    DarkMode touch and click

--- src/js/modules/darkmode.js
+++ src/js/modules/darkmode.js
@@ -1,52 +1,56 @@
-import Lottie from "lottie-web";
+import Lottie from 'lottie-web';
 export default class DarkMode {
   constructor(targetClick, elements, homeGif, logo) {
     this.moon = document.querySelector(targetClick);
     this.sections = document.querySelector(elements);
     this.homeGif = document.querySelector(homeGif);
     this.logo = document.querySelector(logo);
-    this.eventos = ["click", "touchstart"];
+    this.eventos = ['click', 'touchstart'];
     this.changeIcon = this.changeIcon.bind(this);
     Lottie.setSpeed(2.5);
     Lottie.loadAnimation({
       container: this.moon,
-      renderer: "svg",
+      renderer: 'svg',
       loop: false,
       autoplay: false,
-      path: "./img/iconDark.json",
+      path: './img/iconDark.json',
     });
-    Lottie.setDirection("1");
+    Lottie.setDirection('1');
     Lottie.setSpeed(1.7);
   }
 
   changeMode() {
-    let dataTheme = this.sections.getAttribute("data-theme");
+    let dataTheme = this.sections.getAttribute('data-theme');
     let newDataTheme;
-    newDataTheme = dataTheme === "light" ? "dark" : "light";
-    this.sections.setAttribute("data-theme", newDataTheme);
+    newDataTheme = dataTheme === 'light' ? 'dark' : 'light';
+    this.sections.setAttribute('data-theme', newDataTheme);
     this.setLocalDarkMode(newDataTheme);
   }
 
   initCheckLocal() {
-    let local = localStorage.getItem("theme");
-    if (local === "light" || local === null) {
-      this.logo.src = "./img/logo.svg";
-      this.homeGif.src = "./img/homeDarkV2.png";
-      this.moon.addEventListener("click", () => {
-        Lottie.setDirection("1");
-        Lottie.play();
+    let local = localStorage.getItem('theme');
+    if (local === 'light' || local === null) {
+      this.logo.src = './img/logo.svg';
+      this.homeGif.src = './img/homeDarkV2.png';
+      this.eventos.forEach((event) => {
+        this.moon.addEventListener(event, () => {
+          Lottie.setDirection('1');
+          Lottie.play();
+        });
       });
-    } else if (local === "dark") {
-      Lottie.setDirection("1");
+    } else if (local === 'dark') {
+      Lottie.setDirection('1');
       Lottie.play();
-      this.moon.addEventListener("click", () => {
-        Lottie.setSpeed(2.5);
-        Lottie.setDirection("-1");
-        Lottie.play();
+      this.eventos.forEach((event) => {
+        this.moon.addEventListener(event, () => {
+          Lottie.setSpeed(2.5);
+          Lottie.setDirection('-1');
+          Lottie.play();
+        });
       });
 
-      this.logo.src = "./img/logoDark.svg";
-      this.homeGif.src = "./img/homeLight.png";
+      this.logo.src = './img/logoDark.svg';
+      this.homeGif.src = './img/homeLight.png';
     }
   }
 
@@ -55,35 +59,39 @@ export default class DarkMode {
       event.preventDefault();
     }
 
-    let local = localStorage.getItem("theme");
-    this.homeGif.style.webkitTransition = "opacity 10s ease-in-out";
-    this.homeGif.style.MozTransition = "opacity 10s ease-in-out";
-    this.homeGif.style.msTransition = "opacity 10s ease-in-out";
-    this.homeGif.style.oTransition = "opacity 10s ease-in-out";
-    this.homeGif.style.transition = "opacity 10s ease-in-out";
+    let local = localStorage.getItem('theme');
+    this.homeGif.style.webkitTransition = 'opacity 10s ease-in-out';
+    this.homeGif.style.MozTransition = 'opacity 10s ease-in-out';
+    this.homeGif.style.msTransition = 'opacity 10s ease-in-out';
+    this.homeGif.style.oTransition = 'opacity 10s ease-in-out';
+    this.homeGif.style.transition = 'opacity 10s ease-in-out';
 
-    if (local === "light") {
-      this.moon.addEventListener("click", () => {
-        Lottie.setSpeed(2.5);
-        Lottie.setDirection("-1");
-        Lottie.play();
+    if (local === 'light') {
+      this.eventos.forEach((event) => {
+        this.moon.addEventListener(event, () => {
+          Lottie.setSpeed(2.5);
+          Lottie.setDirection('-1');
+          Lottie.play();
+        });
       });
-      this.logo.src = "./img/logoDark.svg";
-      this.homeGif.style.opacity = "0";
+      this.logo.src = './img/logoDark.svg';
+      this.homeGif.style.opacity = '0';
       setTimeout(() => {
-        this.homeGif.src = "./img/homeLight.png";
-        this.homeGif.style.opacity = "1";
+        this.homeGif.src = './img/homeLight.png';
+        this.homeGif.style.opacity = '1';
       }, 250);
-    } else if (local === "dark") {
-      this.moon.addEventListener("click", () => {
-        Lottie.setDirection("1");
-        Lottie.play();
+    } else if (local === 'dark') {
+      this.eventos.forEach((event) => {
+        this.moon.addEventListener(event, () => {
+          Lottie.setDirection('1');
+          Lottie.play();
+        });
       });
-      this.logo.src = "./img/logo.svg";
-      this.homeGif.style.opacity = "0";
+      this.logo.src = './img/logo.svg';
+      this.homeGif.style.opacity = '0';
       setTimeout(() => {
-        this.homeGif.src = "./img/homeDarkV2.png";
-        this.homeGif.style.opacity = "1";
+        this.homeGif.src = './img/homeDarkV2.png';
+        this.homeGif.style.opacity = '1';
       }, 250);
     }
     this.changeMode();
@@ -96,17 +104,17 @@ export default class DarkMode {
   }
 
   setLocalDarkMode(theme) {
-    localStorage.setItem("theme", theme);
+    localStorage.setItem('theme', theme);
   }
 
   checkLocalDarkMode() {
     let local;
-    if (localStorage.getItem("theme") == null) {
-      local = document.documentElement.getAttribute("data-theme");
+    if (localStorage.getItem('theme') == null) {
+      local = document.documentElement.getAttribute('data-theme');
     } else {
-      local = localStorage.getItem("theme");
+      local = localStorage.getItem('theme');
     }
-    document.documentElement.setAttribute("data-theme", local);
+    document.documentElement.setAttribute('data-theme', local);
   }
 
   init() {
